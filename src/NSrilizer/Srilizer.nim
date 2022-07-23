d1ab8b176bdd57a30eb8397ddb6a3bfbf1ac8859
f <fatholah@gmail.com>
2022-07-24 03:23:53 +0430

    desrilizer work with pointer and function overloading but i dont like it

--- src/NSrilizer/Srilizer.nim
+++ src/NSrilizer/Srilizer.nim
@@ -39,6 +39,12 @@ proc toJson*(t:DateTime):JsonNode =
 proc toJson*(t:ptr string):JsonNode =
   return JsonNode(kind:JString,str:t[])
 
+proc fromJson*(T:typedesc[string];t:ptr string,js:JsonNode) =
+  t[]=js.str
+
+
+proc fromJson*(t:ptr string,js:JsonNode) =
+  t[]=js.str
 
 proc toJson*[T](t:ptr seq[T]):JsonNode =
   result=JsonNode(kind:JArray)
@@ -47,7 +53,17 @@ proc toJson*[T](t:ptr seq[T]):JsonNode =
       result.add toJson(t[][i])
     else:
       result.add toJson(t[][i].addr)
-  
+
+proc fromJsonS*[T](t:ptr seq[T],js:JsonNode) =
+  t[].setlen(js.elems.len)
+  for i in 0..<js.elems.len:
+    T.fromJson(t[][i].addr,js.elems[i]  )
+
+proc fromJson*[T](t:ptr seq[T],js:JsonNode) =
+  t[].setlen(js.elems.len)
+  for i in 0..<js.elems.len:
+    fromJson(t[][i].addr,js.elems[i]  )
+
 #[method toJson*(t:BaseType):JsonNode {.base.}=
   return parseJson("""{"$type": \"BaseType\"}""")]#
 
@@ -72,12 +88,13 @@ macro dot*(obj: object, fld: string): untyped =
 
 
 template defineToJsonP*(T:type)=
-  proc toJson(t:ptr T):JsonNode=
+  proc toJson*(t:ptr T):JsonNode=
     result=JsonNode(kind:JObject) #,fields:{: t.getName.toJson}.toOrderedTable)
     when T  is ref|ptr:
       result.add("$type",t.getName.toJson)
     enumAllSerializedFields(T):
-      echo fieldName  ,": ",FieldType ," : "
+      static:
+        echo fieldName  ,": ",FieldType ," : "
       
       when FieldType  is ref|ptr:
         var tmp= t.dot($(fieldName))
@@ -88,21 +105,38 @@ template defineToJsonP*(T:type)=
       else:
         var tmp= t.dot($(fieldName)).addr
         result.add(fieldName,tmp.toJson())
-  proc fromJson(t:T,js:JsonNode):JsonNode=
-    #result=cast
-    
+  proc fromJson*(t: ptr T,js:JsonNode)=
+
     enumAllSerializedFields(T):
       echo fieldName  ,": ",FieldType ," : "
       
       when FieldType  is ref|ptr:
-        var tmp= t.dot($(fieldName))
-        if tmp != nil:
-            result.add(fieldName,tmp.toJson())
-        else:
-            echo "nil"    
+        var tmp=FieldType()
+        fromJson(tmp.addr,js[fieldName])
+        t.dot($(fieldName))=tmp
       else:
-        var tmp= t.dot($(fieldName)).addr
-        result.add(fieldName,tmp.toJson())
+        var tmpP= t.dot($(fieldName)).addr
+        fromJson(tmpP,js[fieldName])
+
+#[proc fromJson*(T:type;t: ptr T,js:JsonNode)=
+  
+  enumAllSerializedFields(T):
+    static:
+      echo "========================================="
+      echo fieldName  ,": ",FieldType ," : "
+    
+    when FieldType  is ref|ptr:
+      var tmp=FieldType()
+      t.dot($(fieldName))=tmp
+      FieldType.fromJson(tmp,js[fieldName])
+    else:
+      when FieldType  is seq:
+        var tmpP= t.dot($(fieldName)).addr
+        fromJsonS(tmpP,js[fieldName])
+      else:
+        var tmpP= t.dot($(fieldName)).addr
+        FieldType.fromJson(tmpP,js[fieldName])
+]# 
 
 
 
